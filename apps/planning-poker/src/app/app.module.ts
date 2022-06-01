17a0399fb30421e3edac2489c404f681ec2f90c4
= <=>
2022-06-01 23:21:25 +0200

    add the header (ongoing)

--- apps/planning-poker/src/app/app.module.ts
+++ apps/planning-poker/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderModule } from './shared/components/header/header.module';
+import { RippleModule } from 'primeng/ripple';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RippleModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
