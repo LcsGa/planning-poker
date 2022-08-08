fcb7b83da7c9f3371c06fba287fd7e73a494362b
LcsGa <lcs.grc@outlook.com>
2022-08-08 22:47:23 +0200

    statically serve the app from the server

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,14 +2,23 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 
+import { ServeStaticModule } from "@nestjs/serve-static";
+
 import { LobbyGateway } from "./lobby/lobby.gateway";
 
 import { LobbyService } from "./lobby/lobby.service";
 
 import { FormatResultsPipe } from "./lobby/pipes/format-results.pipe";
 
+import { join } from "path";
+
 @Module({
   controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "planning-poker"),
+    }),
+  ],
   providers: [FormatResultsPipe, LobbyGateway, LobbyService],
 })
 export class AppModule {}
