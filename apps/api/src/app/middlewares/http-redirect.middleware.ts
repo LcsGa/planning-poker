d585ac3aa2c460ff9ec6ffe62db54e6cfd595472
LcsGa <lcs.grc@outlook.com>
2022-08-14 23:19:12 +0200

    fix: add the https port on the redirection middleware

--- apps/api/src/app/middlewares/http-redirect.middleware.ts
+++ apps/api/src/app/middlewares/http-redirect.middleware.ts
@@ -1,11 +1,12 @@
-import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class HttpRedirectMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (!req.secure) {
-      const httpsUrl = `https://${req.hostname}${req.originalUrl}`;
+      const httpsUrl = `https://${req.hostname}:${environment.httpsPort}${req.originalUrl}`;
       res.redirect(HttpStatus.PERMANENT_REDIRECT, httpsUrl);
     } else {
       next();
