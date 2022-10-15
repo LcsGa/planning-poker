610afd086ce554deaff5c4131cb4d3c5f31861aa
LcsGa <lcs.grc@outlook.com>
2022-10-15 11:35:26 +0200

    [#6] refactor: make the theme fully reactive

--- apps/planning-poker/src/app/shared/components/header/header.component.ts
+++ apps/planning-poker/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { ThemeService } from "../../services/theme.service";
 import { Icon } from "../../utils/icon.utils";
 
 @Component({
